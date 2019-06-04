78ac9fa6661bcaf4355ecc582150d814ed5a4be3
Praveen Naik <praveenhnvr@gmail.com>
2019-06-04 13:21:23 +0530

    Class added

---
+++ src/Demo3.java
@@ -0,0 +1,12 @@
+
+public class Demo3 {
+
+	public static void main(String[] args) {
+	System.out.println("hi");
+	System.out.println("hi");
+	System.out.println("hi");
+	System.out.println("hi");
+
+	}
+
+}
